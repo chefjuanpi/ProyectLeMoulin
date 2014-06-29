3329bf4457fa6a08ba7ed5dcfa3ea17a9ed4a2dd
chefjuanpi <chocolateria.arriba@gmail.com>
2014-06-29 16:35:53 -0400

    cote public presque termine
    metre a jour la base de donnes avant de comencer a travailler sur le
    programe

---
+++ LeMoulinDaCote/Models/MailModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeMoulinDaCote.Models
+{
+    public class MailModel
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        
+    }
+}
