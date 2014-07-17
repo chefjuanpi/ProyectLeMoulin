684b0fd47e332f379f48531f26eef24a509a236b
grizzlyzord <mazinkaizer@live.ca>
2014-07-17 16:50:28 -0400

    Misa a jour Steeve
    Création Controleur Epicerie et View Groupe d'achat (membres)

---
+++ ProyectLeMoulin/Controllers/Epicerie.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using ProyectLeMoulin.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectLeMoulin.Controllers
+{
+
+    public class EpicerieController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetCategories()
+        {
+            CoeurContainer db = new CoeurContainer();
+
+            var category = (from c in db.Categories
+                            select new
+                            {
+                                CategoryID = c.CategoryID,
+                                CategoryName = c.CategoryName,
+                                Description = category.Description
+                            }).single();
+            return Json(category, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetProduits(int cat)
+        {
+            CoeurContainer db = new CoeurContainer();
+
+            var produit = (from p in db.Produits
+                           where p.Categories.CategoryID == cat
+                           select new
+                           {
+                               ProductID = p.ProductID,
+                               ProductName = p.ProductName,
+                               Description = p.Description,
+                               TVQ = p.TVQ,
+                               TPS = p.TPS,
+                               Available = p.Available
+                           }).ToList();
+            return Json(produit, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
