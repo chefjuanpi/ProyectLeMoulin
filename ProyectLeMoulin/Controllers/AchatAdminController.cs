453dcab77ad2bdb9a10bf1cd8ee8c9160ac8aafe
Goym06 <goym06@hotmail.com>
2014-08-07 11:37:05 -0400

    simple arborescence fonctionnel
    Definir l'utilité de "Liste d'achat"? Elle n'est pas dans l'analyse

---
+++ ProyectLeMoulin/Controllers/AchatAdminController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectLeMoulin.Controllers
+{
+    public class AchatAdminController : Controller
+    {
+        //
+        // GET: /AchatAdmin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewOrder()
+        {
+            ViewBag.message = "Pour faire une nouvelle commande";
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            ViewBag.message = "Administration des produits";
+            return View();
+        }
+
+        public ActionResult Suppliers()
+        {
+            ViewBag.message = "Administration des fournisseurs";
+            return View();
+        }
+
+        public ActionResult OldOrders()
+        {
+            ViewBag.message = "Pour obtenir les rapports d'anciennes commandes";
+            return View();
+        }
+
+        public ActionResult MembersOrders()
+        {
+            ViewBag.message = "Pour sortir les commandes des membres et leurs reçu";
+            return View();
+        }
+    }
+}
