aa371b0c96a29766668d2d59187e77cf9aba1851
pablo Aguilar Lliguin <chocolateria.arriba@gmail.com>
2014-08-11 11:12:21 -0400

    Merge remote-tracking branch 'origin/master'
    ahahahah

---
+++ ProyectLeMoulin/Controllers/Panier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectLeMoulin.Controllers
+{
+    class Panier
+    {
+        public int ProductID { get; set; }
+
+        public string Produits { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Qantity { get; set; }
+    }
+}
