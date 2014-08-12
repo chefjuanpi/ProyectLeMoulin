9530e90ec7c19d7ed31fa0780a71240429ed4091
grizzlyzord <estive24>
2014-08-11 20:04:18 -0400

    steeve - affichage

--- ProyectLeMoulin/Controllers/EpicerieController.cs
+++ ProyectLeMoulin/Controllers/EpicerieController.cs
@@ -17,9 +17,6 @@ namespace ProyectLeMoulin.Controllers
     public class EpicerieController : Controller
     {
 
-
-
-
         public ActionResult Index()
         {
 
@@ -42,83 +39,93 @@ namespace ProyectLeMoulin.Controllers
         }
 
         //Récupérer les produits selon la catégorie choisie
-        //public JsonResult GetProduits(string cat)
-        //{
-        //    EpicerieEntities db = new EpicerieEntities();
+        public JsonResult GetProduits(string cat)
+        {
+            EpicerieEntities db = new EpicerieEntities();
 
-        //    var produit = (from w in db.Week
-        //                   join p in db.Products
-        //                   on w.ProductId equals p.ProductId
-        //                   join c in db.Categories
-        //                   on cat equals c.CategoryId
-        //                   where c.CategoryName == cat
-        //                   select new
-        //                   {
-        //                       ProductID = w.ProductId,
-        //                       ProductName = w.Products,
-        //                       TVQ = p.TVQ,
-        //                       TPS = p.TPS,
-        //                       Available = p.Avaibled
-        //                   }).ToList();
-        //    return Json(produit, JsonRequestBehavior.AllowGet);
-        //}
+            var produit = (from w in db.Week
+                           join p in db.Products
+                           on w.ProductId equals p.ProductId
+                           join c in db.Categories
+                           on cat equals c.CategoryName
+                           where c.CategoryName == cat
+                           select new
+                           {
+                               ProductID = w.ProductId,
+                               ProductName = w.Products,
+                               TVQ = p.TVQ,
+                               TPS = p.TPS,
+                               Available = p.Avaibled
+                           }).ToList();
+            return Json(produit, JsonRequestBehavior.AllowGet);
+        }
 
         //Remplir le Panier
-        public JsonResult Remplir_Le_Panier(int productId)
+        public JsonResult Remplir_Le_Panier(int productId, string produit, int qty, decimal price)
         {
             Panier p = new Panier();
 
-            int X = 0;
-
-            p.ProductID =   X;
-            p.Produits  =   "aa";
-            p.Qantity   =   X;
-            p.Price     =   X;
+            p.ProductID = productId;
+            p.Produits = produit;
+            p.Qantity = qty;
+            p.Price = price;
 
             return Json(p, JsonRequestBehavior.AllowGet);
 
         }
-        
+
         //Afficher le panier d'épicerie
-        public JsonResult Afficherer_Le_Panier()
-        {
-            EpicerieEntities db = new EpicerieEntities();
+        //public JsonResult Afficherer_Le_Panier()
+        //{
+        //    Panier p = new Panier();
 
-            var panier = (from w in db.Week
-                          select new Panier()
-                          {
-                              ProductID     =   w.ProductId,
-                              Produits      =   "",
-                              Price         =   w.UnitPrice,
-                              Qantity       =   w.Quantity
-                          }).ToList();
+        //    var panier = p;
 
-            return Json(panier, JsonRequestBehavior.AllowGet);
-        }
+        //    return Json(panier, JsonRequestBehavior.AllowGet);
+
+
+        //}
 
         //Récupérer le GUID du membre connecté et son role au sein du groupe d'achats
         //public JsonResult Recuperer_Membre()
         //{
 
-        //    EpicerieEntities db = new EpicerieEntities();
-        //    string utilisateur = User.Identity.Name;
+        //    EpicerieEntities db   = new EpicerieEntities();
+        //    string utilisateur    = User.Identity.Name;
         //    string guid = db.AspNetUsers.Single(m => m.UserName == utilisateur).Id;
 
-        //    var membre = (from m in db.AspNetUsers
-        //                  join r in db.AspNetUserRoles
-        //                  on m.UserName equals r.AspNetUsers
-        //                  where m.Id == guid
+        //    var membre = (from    m       in      db.AspNetUsers
+        //                  join    r       in      db.AspNetUserRoles
+        //                  on      m.Id    equals  r.UserId
+        //                  where   m.Id    ==      guid
         //                  select new
         //                  {
-        //                      MembreID          =   m.Id,
-        //                      UserFirstName     =   m.Prenom, 
-        //                      UserLastName      =   m.Nom,
-        //                      Role              =   r.RoleId
+        //                      MembreID        =   m.Id,
+        //                      UserFirstName   =   m.Prenom, 
+        //                      UserLastName    =   m.Nom,
+        //                      Role            =   r.RoleId
         //                  }).ToList();
-        //        return Json(membre, JsonRequestBehavior.AllowGet); 
-        //    }
+        //    return Json(membre, JsonRequestBehavior.AllowGet); 
         //}
 
-        
+        //Afficher le message de l'administrateur du groupe D'achats
+        //public JsonResult Afficher_Message()
+        //{
+
+        //}
+
+        //Valider la date de l'exercice d'achat en cours
+        //public JsonResult Validerer_Date()
+        //{
+        //    EpicerieEntities db = new EpicerieEntities();
+
+        //    var semaine = (from w in db.Week
+        //                    select new
+        //                    {
+        //                        Semaine   =   db.
+        //                    }).ToString;
+        //    return Json(semaine, JsonRequestBehavior.AllowGet);
+        //}
+
     }
 }
