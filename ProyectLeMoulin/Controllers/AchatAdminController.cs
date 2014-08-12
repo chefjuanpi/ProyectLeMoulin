ab5639c97f7f1fe1bb8a41c3f09fa68c0a392890
Goym06 <goym06@hotmail.com>
2014-08-12 15:48:44 -0400

    Supplier 95%

--- ProyectLeMoulin/Controllers/AchatAdminController.cs
+++ ProyectLeMoulin/Controllers/AchatAdminController.cs
@@ -48,91 +48,90 @@ namespace ProyectLeMoulin.Controllers
             return View();
         }
 
-//        [HttpPost]
-//        [ValidateInput(false)]
-//        public async Task<ActionResult> Supplier(SupplierViewModel Supplier)
-//        {
-//            EpicerieEntities db = new EpicerieEntities();
-//            if (Supplier.SupplierId == null)
-//            {
-//                Suppliers s = new Suppliers();
-//                s.SupplierName = Supplier.SupplierName;
-//                s.ContactName = Supplier.SupplierContactName;
-//                s.Adress = Supplier.SupplierAdress;
-//                s.E_Mail = Supplier.SupplierMail;
-//                s.Phone = Supplier.SupplierPhone;
-//                s.Fax = Supplier.SupplierFax;
-//                s.PostalCode = Supplier.SupplierPostalCode;
-//                s.Ville = Supplier.SupplierCity;
+        [HttpPost]
+        public ActionResult Supplier(SupplierViewModel Supplier)
+        {
+            EpicerieEntities db = new EpicerieEntities();
+            if (Supplier.SupplierId == null)
+            {
+                Suppliers s = new Suppliers();
+                s.SupplierName = Supplier.SupplierName;
+                s.ContactName = Supplier.SupplierContactName;
+                s.Adress = Supplier.SupplierAdress;
+                s.E_Mail = Supplier.SupplierMail;
+                s.Phone = Supplier.SupplierPhone;
+                s.Fax = Supplier.SupplierFax;
+                s.PostalCode = Supplier.SupplierPostalCode;
+                s.Ville = Supplier.SupplierCity;
 
-//                db.Suppliers.Add(s);
-//                await db.SaveChangesAsync();
-//            }
-//            else
-//            {
-//                int x = Convert.ToInt16(Supplier.SupplierId);
-//                var modifsupplier = (from s in db.Suppliers
-//                                   where s.SupplierId == x
-//                                   select s).Single();
-//                modifsupplier.SupplierName = Supplier.SupplierName;
-//                modifsupplier.ContactName = Supplier.SupplierContactName;
-//                modifsupplier.Adress = Supplier.SupplierAdress;
-//                modifsupplier.E_Mail = Supplier.SupplierMail;
-//                modifsupplier.Phone = Supplier.SupplierPhone;
-//                modifsupplier.Fax = Supplier.SupplierFax;
-//                modifsupplier.PostalCode = Supplier.SupplierPostalCode;
-//                modifsupplier.Ville = Supplier.SupplierCity;
-//                await db.SaveChangesAsync();
-//            }
-//            return View();
-//        }
+                db.Suppliers.Add(s);
+                db.SaveChanges();
+            }
+            else
+            {
+                int x = Convert.ToInt16(Supplier.SupplierId);
+                var modifsupplier = (from s in db.Suppliers
+                                     where s.SupplierId == x
+                                     select s).Single();
+                modifsupplier.SupplierName = Supplier.SupplierName;
+                modifsupplier.ContactName = Supplier.SupplierContactName;
+                modifsupplier.Adress = Supplier.SupplierAdress;
+                modifsupplier.E_Mail = Supplier.SupplierMail;
+                modifsupplier.Phone = Supplier.SupplierPhone;
+                modifsupplier.Fax = Supplier.SupplierFax;
+                modifsupplier.PostalCode = Supplier.SupplierPostalCode;
+                modifsupplier.Ville = Supplier.SupplierCity;
+                db.SaveChanges();
+            }
+            return View();
+        }
 
-//        public JsonResult getSuppliers()
-//        {
-//            EpicerieEntities db = new EpicerieEntities();
-//            var suppliers = (from s in db.Suppliers
-//                             orderby s.SupplierName
-//                             select new
-//                             {
-//                                 id = s.SupplierId,
-//                                 nom = s.SupplierName
-//                             });
-//            return Json(suppliers, JsonRequestBehavior.AllowGet);
-//        }
+        public JsonResult getSuppliers()
+        {
+            EpicerieEntities db = new EpicerieEntities();
+            var suppliers = (from s in db.Suppliers
+                             orderby s.SupplierName
+                             select new
+                             {
+                                 id = s.SupplierId,
+                                 nom = s.SupplierName
+                             });
+            return Json(suppliers, JsonRequestBehavior.AllowGet);
+        }
 
-//        public JsonResult getSupplierDetails(int SupplierId)
-//        {
-//            EpicerieEntities db = new EpicerieEntities();
-//            var supplier = (from s in db.Suppliers
-//                            where s.SupplierId == SupplierId
-//                            select new
-//                            {
-//                                id = s.SupplierId,
-//                                Nom = s.SupplierName,
-//                                contact = s.ContactName,
-//                                adresse = s.Adress,
-//                                mail = s.E_Mail,
-//                                phone = s.Phone,
-//                                fax = s.Fax,
-//                                codepostal = s.PostalCode,
-//                                ville = s.Ville,
-//                            }).Single();
-//            return Json(supplier, JsonRequestBehavior.AllowGet);
-//        }
+        public JsonResult getSupplierDetails(int SupplierId)
+        {
+            EpicerieEntities db = new EpicerieEntities();
+            var supplier = (from s in db.Suppliers
+                            where s.SupplierId == SupplierId
+                            select new
+                            {
+                                id = s.SupplierId,
+                                Nom = s.SupplierName,
+                                contact = s.ContactName,
+                                adresse = s.Adress,
+                                mail = s.E_Mail,
+                                phone = s.Phone,
+                                fax = s.Fax,
+                                codepostal = s.PostalCode,
+                                ville = s.Ville,
+                            }).Single();
+            return Json(supplier, JsonRequestBehavior.AllowGet);
+        }
 
-//        public async Task<ActionResult> delSupplier(int nID)
-//        {
-//            EpicerieEntities db = new EpicerieEntities();
-//            var delsupplier = (from n in db.Suppliers
-//                               where n.SupplierId == nID
-//                             select n).Single();
-//            if (delsupplier != null)
-//            {
-//                db.Suppliers.Remove(delsupplier);
-//                await db.SaveChangesAsync();
-//            }
-//            return Redirect("/Admin/Suppliers");
-//        }
+        public ActionResult delSupplier(int nID)
+        {
+            EpicerieEntities db = new EpicerieEntities();
+            var delsupplier = (from s in db.Suppliers
+                               where s.SupplierId == nID
+                               select s).Single();
+            if (delsupplier != null)
+            {
+                db.Suppliers.Remove(delsupplier);
+                db.SaveChanges();
+            }
+            return Redirect("/AchatAdmin/Suppliers");
+        }
 
     }
 }
