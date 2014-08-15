1bc2c5a0e52fdb8802270be45f27823b96c850c0
pablo Aguilar Lliguin <chocolateria.arriba@gmail.com>
2014-08-15 16:36:19 -0400

    BD actualisation!! page Accueil 75%
    gallerie des evenements términé

--- ProyectLeMoulin/Controllers/HomeController.cs
+++ ProyectLeMoulin/Controllers/HomeController.cs
@@ -24,19 +24,25 @@ namespace IdentitySample.Controllers
                           orderby e.DateStart
                           select new
                           {
-                              photos = e.PrincipalPhotoEvenement
+                              photos = e.PrincipalPhotoEvenement,
+                              titre = e.TitleEvenement,
+                              dateStart = e.DateStart,
+                              dateEnd = e.DateEnd
                           }).ToList();
             string t = "";
             for (int i = 0; i < photos.Count; i++)
             {
-                t += "<img href='../tinyfilemanager.net/resources/files/" + photos[i].photos + 
-                    "'" + "src='../tinyfilemanager.net/resources/thumbs/" + photos[i].photos + 
-                    "'>";
+                t += "<div data-img='../tinyfilemanager.net/resources/files/" + photos[i].photos +
+                    "' >" +
+                    "<div><div class='cubobajatexto'></div> <a class='fotorama__select' href='/Home/Evenement?id=" + photos[i].titre + "'>" +
+                    "<div class='alert alert-info' style=' background:#464646; background-color: rgba(91, 192, 222, 0.5);' ><div class='row'><h2>" + photos[i].titre + "</h1></div><div class='row'><h2>de " +
+                    photos[i].dateStart.ToLongDateString() + " au " + photos[i].dateEnd.ToLongDateString() + "</h2></div></a></div></div></div>";
+
+
             }
-            t += "";
-            int o = 0;
             return Content(t);
         }
+
 
         public ActionResult About()
         {
