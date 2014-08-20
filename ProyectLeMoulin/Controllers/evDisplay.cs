14f921c9a3ef15cee8adb82d2e71cb631bbd8dcf
pablo Aguilar Lliguin <chocolateria.arriba@gmail.com>
2014-08-20 16:33:55 -0400

    ups le control

---
+++ ProyectLeMoulin/Controllers/evDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IdentitySample.Controllers
+{
+    class evDisplay
+    {
+        public DateTime date { get; set; }
+
+        public string type { get; set; }
+
+        public TimeSpan heureStart { get; set; }
+
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        public string url { get; set; }
+
+        public DateTime datefin { get; set; }
+
+        public TimeSpan heurefin { get; set; }
+    }
+}
