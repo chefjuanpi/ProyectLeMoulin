2f53e5d1710c2e329727be5533aaa1b8a2c7de75
grizzlyzord <mazinkaizer@live.ca>
2014-08-25 13:13:16 -0400

    Affichage des prix corrigé

--- ProyectLeMoulin/Controllers/EpicerieController.cs
+++ ProyectLeMoulin/Controllers/EpicerieController.cs
@@ -10,19 +10,16 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Collections.Generic;
 
 namespace IdentitySample.Controllers
 {
-    
+
     public class EpicerieController : Controller
+    
     {
         EpicerieEntities db = new EpicerieEntities();
-        
-        public decimal Total = 0;
-        public decimal TotTaxe = 0;
-        public decimal GrandTot = 0;
-        public decimal Quebec = 0;
-        public decimal Canada = 0;
+        //public Panier x = new Panier();
 
         public ActionResult Index()
         {
@@ -35,13 +32,18 @@ namespace IdentitySample.Controllers
         public async Task<ActionResult> Index(test1234 panier)
         {
             //pour garder ou modifier la bd, async xq sea mas rapido para el utilisateur
+           
 
             ViewBag.Steeve = "ton panier";
             return View();
 
         }
 
-        //Récupérer les catégories de produits
+        
+        /// <summary>
+        /// Récupérer les catégories de produits
+        /// </summary>
+        /// <returns></returns>
         public JsonResult GetCategories()
         {
             //EpicerieEntities db = new EpicerieEntities();
@@ -55,7 +57,11 @@ namespace IdentitySample.Controllers
             return Json(category, JsonRequestBehavior.AllowGet);
         }
 
-        //Récupérer les produits selon la catégorie choisie
+        /// <summary>
+        /// Récupérer les produits selon la catégorie choisie
+        /// </summary>
+        /// <param name="cat"></param>
+        /// <returns></returns>
         public JsonResult GetProduits(int cat)
         {
             
@@ -78,105 +84,6 @@ namespace IdentitySample.Controllers
                 return Json(produit, JsonRequestBehavior.AllowGet);
             //}
         }
-
-        /// <summary>
-        /// Remplir le Panier
-        /// </summary>
-        /// <param name="prod">ProductID sélectionné</param>
-        /// <param name="qty">Quantité choisie</param>
-        /// <returns>retourne le contenu du panier</returns>
-        //public JsonResult Remplir_Le_Panier(int prod)
-        //{
-            
-            
-        //    //if (prod == db.Products.)
-        //    //{
-        //        var product = (from w in db.Week
-        //                       join p in db.Products
-        //                       on w.ProductId equals p.ProductId
-        //                       join cp in db.CategoryProduct
-        //                       on p.ProductId equals cp.ProductId
-        //                       where p.ProductId == prod
-        //                       join od in db.OrderDetails
-        //                       on w.ProductId equals od.ProductId
-        //                       join o in db.Orders
-        //                       on od.OrderId equals o.OrderId
-        //                       select new
-        //                       {
-        //                            ProductId = w.ProductId,
-        //                            ProductName = p.ProductName,
-        //                            //Qantity = qty,
-        //                            Price = w.UnitPrice,
-        //                            CategoryID = cp.CategoryId
-        //                       }).ToList();
-
-        //        return Json(product, JsonRequestBehavior.AllowGet);
-        //    //}
-        //}
-
-        
-
-        /// <summary>
-        /// Supprimer un article du panier
-        /// </summary>
-        /// <param name="prod">ProductId sélectionné</param>
-        /// <returns>Contenu du panier après la supression</returns>
-        //public ActionResult delArticle(int prod)
-        //{
-        //    var delsupplier = (from a in Panier
-        //                       where a.ProductId == prod
-        //                       select new Panier()
-        //                       ).ToList();
-        //    if (delArticle != null)
-        //    {
-        //        a.Remove(delArticle);
-        //        a.SaveChanges();
-        //    }
-        //    return Redirect("/Epicerie/Remplir_Le_Panier");
-        //}
-
-        //public void Calculer_Prix_Total(bool tvq, bool tps, decimal price)
-        //{
-
-        //    if (tvq == true && tps == true)
-        //    {
-        //        p.Quebec = P.Price * (decimal)0.09975;
-        //        p.Canada = P.Price * (decimal)0.05;
-        //        p.Total = P.Price + p.Quebec + p.Canada;
-
-        //    }
-        //    if (tvq == true && tps == false)
-        //    {
-        //        p.Quebec = P.Price * (decimal)0.09975;
-        //        p.Canada = 0;
-        //        p.Total = P.Price + p.Quebec;
-        //    }
-
-        //    if (tps == true && tvq == false)
-        //    {
-        //        P.Canada = price * (decimal)0.05;
-        //        P.Quebec = 0;
-        //        p.Total = P.Price + p.Canada;
-        //    }
-        //    if (tvq == false && tps == false)
-        //    {
-        //        p.Quebec = 0;
-        //        p.Canada = 0;
-        //        return;
-        //    }
-        //}
-
-        /// <summary>
-        /// Affiche le contenu du panierpour confirmation
-        /// </summary>
-        //public JsonResult Afficher_Le_Panier()
-        //{
-        //    Panier p = new Panier();
-
-        //    var panier = p;
-
-        //    return Json(panier, JsonRequestBehavior.AllowGet);
-        //}
 
         /// <summary>
         /// Récupérer le GUID du membre connecté et son role au sein du groupe d'achats
---
+++ ProyectLeMoulin/Models/Panier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentitySample.Models
+{
+    public class Panier
+    {
+        public int id;
+        public int qty;
+    }
+}
